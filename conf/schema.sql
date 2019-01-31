effbe916b535e3a98d5af4bc9ce651f4929fffa5
eleve <eleve@STAEL-109-09>
2019-01-31 14:57:14 +0100

    Initial commit

---
+++ conf/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user
+(
+	username VARCHAR(64) NOT NULL,
+	passwd_hash VARCHAR(64) NOT NULL,
+	first_name VARCHAR(64) DEFAULT NULL,
+	last_name VARCHAR(64) DEFAULT NULL,
+	email VARCHAR(128) DeFAULT NULL
+); 
+
+INSERT INTO user (username, passwd_hash, first_name, last_name, email) VALUES ('Nat','2e8c0277e396fabf683e56c8b7fa7e6dad68c679','Nathan','Arpin','arpinnathan@gmail.com');
